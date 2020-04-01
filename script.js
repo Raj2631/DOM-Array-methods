52d2b8d843b75d344bfc5c4d8d0f62b7c8cfa8ef
Rakshit Pathak <rajatpathak435@gmail.com>
2020-04-01 10:44:43 +0530

    renders dom using foreach, added functionality to Add user button.

--- script.js
+++ script.js
@@ -24,8 +24,36 @@ async function getRandomUser() {
     name: `${user.name.first} ${user.name.last}`,
     money: Math.floor(Math.random() * 1000000)
   };
+
+  addData(newUser);
 }
 
+//Add new object to data arr.
 function addData(obj) {
   data.push(obj);
+  updateDOM();
 }
+
+//Update DOM
+function updateDOM(providedData = data) {
+  //Clear main div.
+  main.innerHTML = `<h2><strong>Person</strong> Wealth</h2>`;
+
+  providedData.forEach(item => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `
+    <strong>${item.name}</strong> ${formatMoney(item.money)}
+    `;
+    main.appendChild(element);
+  });
+}
+
+//Format number as money.
+
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+//Event Listeners
+addUserBtn.addEventListener('click', getRandomUser);
