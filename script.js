f9cd21896fbbe5b6b5e79e4f39eae4ed2b06ca79
Rakshit Pathak <rajatpathak435@gmail.com>
2020-04-01 12:25:09 +0530

    Added functionality to sort, show only millionaires, and get total btn

--- script.js
+++ script.js
@@ -36,6 +36,32 @@ function doubleMoney() {
   updateDOM();
 }
 
+//Sort users by richest.
+function sortByRichest() {
+  data.sort((a, b) => b.money - a.money);
+
+  updateDOM();
+}
+
+//Filter only millionaires.
+
+function showMillionaires() {
+  data = data.filter(user => user.money > 1000000);
+  updateDOM();
+}
+
+//Calculate the total wealth.
+
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc + user.money, 0));
+
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong> ${formatMoney(
+    wealth
+  )}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
+
 //Add new object to data arr.
 function addData(obj) {
   data.push(obj);
@@ -64,5 +90,7 @@ function formatMoney(number) {
 
 //Event Listeners
 addUserBtn.addEventListener('click', getRandomUser);
-
+sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
 doubleBtn.addEventListener('click', doubleMoney);
+calculateWealthBtn.addEventListener('click', calculateWealth);
